9bbbb36cfb20af508d82d4de270ec25655b5e235
ConCh3TuXd <conch3tuxd@gmail.com>
2024-11-11 14:03:51 -0500

    file doc pdf and drive

--- flows/carta.flow.js
+++ flows/carta.flow.js
@@ -3,7 +3,8 @@ const { addKeyword, EVENTS } = require('@bot-whatsapp/bot')
 const   cartaFlow = addKeyword(EVENTS.ACTION)        
 
     .addAnswer('Carta', {
-        media: 'https://amitel.com.pe/amitelftp/2024/octubre/C0001637.pdf',                
+        media: 'https://drive.google.com/file/d/1r1xJiYltPBMw2S7MY0pM6zSeXVr0vTvH/view',                
+         //https://amitel.com.pe/amitelftp/2024/octubre/C0001637.pdf
         //media: './CARTA_RESPONDONA.pdf',                        
     })
     .addAnswer(['Aqui nuestra carta.','2. Para iniciar una reserva','0. Volver al menú principal'])    
