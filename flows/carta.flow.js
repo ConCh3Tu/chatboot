8d710ba19b12418c079eb4d9b6edd19c6e31ff42
ConCh3TuXd <conch3tuxd@gmail.com>
2024-11-11 12:27:32 -0500

    edit carta.flow source files

--- flows/carta.flow.js
+++ flows/carta.flow.js
@@ -1,14 +1,15 @@
 const { addKeyword, EVENTS } = require('@bot-whatsapp/bot')
 
-
 const   cartaFlow = addKeyword(EVENTS.ACTION)        
 
     .addAnswer('Carta', {
         body: "carta",        
-        media: 'D:/WampNode/CAFETERIA RAYMI/base-baileys-memory/CARTA_RESPONDONA.pdf',                
+        media: './CARTA_RESPONDONA.pdf',                
         delay: 100        
     })
     .addAnswer(['Aqui nuestra carta.','2. Para iniciar una reserva','0. Volver al menú principal'])    
 
     
 module.exports  =  { cartaFlow };
+
+
